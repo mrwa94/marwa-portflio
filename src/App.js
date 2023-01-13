83495d5470f9ebff0b4e43ed16634291ef99e1d5
Marwa Alsubhi <marwa@Marwas-MacBook-Pro.local>
2023-01-13 17:58:32 +0300

    my website

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Sidebar from './compontes/Sidebar';
+
+
+
+
+function App() {
+  return (
+    <div className="">
+    <Sidebar/>
+
+
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/compontes/Logo.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import image from '../images/logo.png'
+export default function logo() {
+  return (
+    <div>
+        <img src={image}></img>   
+    </div>
+
+  )
+}
